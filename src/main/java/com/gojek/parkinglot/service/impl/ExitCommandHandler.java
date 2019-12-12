861cb172d7feaf60386723c14e289ffa9233a57c
Mohd Nadeem <mohd.nadeem@cars24.com>
2019-12-13 03:37:00 +0530

    exit command handler implemented

--- src/main/java/com/gojek/parkinglot/service/impl/ExitCommandHandler.java
+++ src/main/java/com/gojek/parkinglot/service/impl/ExitCommandHandler.java
@@ -1,6 +1,7 @@
 package com.gojek.parkinglot.service.impl;
 
 import com.gojek.parkinglot.service.CommandHandler;
+import com.gojek.parkinglot.service.ParkingLotService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,9 +14,17 @@ public class ExitCommandHandler implements CommandHandler {
 
     private static final Logger log = LoggerFactory.getLogger(ExitCommandHandler.class);
 
+    private ParkingLotService parkingLotService;
+
+    public ExitCommandHandler(ParkingLotService parkingLotService) {
+        this.parkingLotService = parkingLotService;
+    }
+
     @Override
     public String execute(String[] args) {
         log.info("Executing '{}' command.", args[0]);
+        log.info("Exiting the system.");
+        System.exit(0);
         return "";
     }
 }
