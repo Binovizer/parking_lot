07ca09cb7a448ec83c54ea22c553b3b19d8a31ea
Mohd Nadeem <mohd.nadeem@cars24.com>
2019-12-13 00:03:18 +0530

    project setup with gitignore

---
+++ src/main/java/com/gojek/parkinglot/ParkingLotApplication.java
@@ -0,0 +1,16 @@
+package com.gojek.parkinglot;
+
+import java.util.Arrays;
+
+/**
+ * The type ParkingLotApplication
+ *
+ * @author Mohd Nadeem
+ */
+public class ParkingLotApplication {
+
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.toString(args));
+    }
+
+}
