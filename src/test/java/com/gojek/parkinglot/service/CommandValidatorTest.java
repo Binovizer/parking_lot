aa3c7768e5ea7cc5548b57faf2ad6ca2ec5eed9c
Mohd Nadeem <mohd.nadeem@cars24.com>
2019-12-13 12:53:28 +0530

    validator test cases added

---
+++ src/test/java/com/gojek/parkinglot/service/CommandValidatorTest.java
@@ -0,0 +1,42 @@
+package com.gojek.parkinglot.service;
+
+import com.gojek.parkinglot.exception.CommandArgumentsException;
+import com.gojek.parkinglot.exception.CommandNotSupportedException;
+import com.gojek.parkinglot.service.impl.CommandValidator;
+import com.gojek.parkinglot.utils.CommandSupported;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The type ValidatorTest
+ *
+ * @author Mohd Nadeem
+ */
+class CommandValidatorTest {
+
+    private CommandValidator validator;
+
+    @BeforeEach
+    void init() {
+        validator = new CommandValidator();
+    }
+
+    @Test
+    void validateWithCorrectCommandAndRequiredParameters() {
+        String command = "create_parking_lot 6";
+        Assertions.assertEquals(CommandSupported.CREATE_PARKING_LOT, validator.validate(command));
+    }
+
+    @Test
+    void validateWithIncorrectCommand() {
+        String command = "create_parking 6";
+        Assertions.assertThrows(CommandNotSupportedException.class, () -> validator.validate(command));
+    }
+
+    @Test
+    void validateWithCorrectCommandWithoutRequiredParameters() {
+        String command = "create_parking_lot";
+        Assertions.assertThrows(CommandArgumentsException.class, () -> validator.validate(command));
+    }
+}
